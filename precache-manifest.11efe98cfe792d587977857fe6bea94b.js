001d4a7002628054e37213d26e66c96e7acf0251
impressiveHen <b02505002@gmail.com>
2020-02-12 00:12:04 -0800

    Updates

---
+++ precache-manifest.11efe98cfe792d587977857fe6bea94b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb625c8256dc8bff44fd7fbee7b0451",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "5a85291a2a80cdf00030",
+    "url": "/Pokedex/static/css/main.502aa52b.chunk.css"
+  },
+  {
+    "revision": "c2677611de628387e2b1",
+    "url": "/Pokedex/static/js/2.ebdf0777.chunk.js"
+  },
+  {
+    "revision": "5a85291a2a80cdf00030",
+    "url": "/Pokedex/static/js/main.61cada80.chunk.js"
+  },
+  {
+    "revision": "6661b49ad0834a75d179",
+    "url": "/Pokedex/static/js/runtime-main.34ba02eb.js"
+  },
+  {
+    "revision": "14aba60b06ced6ea846ad63b6611f524",
+    "url": "/Pokedex/static/media/Blastoise.14aba60b.png"
+  },
+  {
+    "revision": "9ba4d684e64c19af7c99148ac36af46b",
+    "url": "/Pokedex/static/media/Bulbasaur.9ba4d684.png"
+  },
+  {
+    "revision": "2d32cf3c3042861de658c96052673408",
+    "url": "/Pokedex/static/media/Charizard.2d32cf3c.png"
+  },
+  {
+    "revision": "9be8f2a81b006e2f8897f1889107d0f7",
+    "url": "/Pokedex/static/media/Charmander.9be8f2a8.png"
+  },
+  {
+    "revision": "4e83c7dbbb912257f06b13cc1468f76f",
+    "url": "/Pokedex/static/media/Charmeleon.4e83c7db.png"
+  },
+  {
+    "revision": "418bae45cd62c468f1c58d268e022c7f",
+    "url": "/Pokedex/static/media/Cosmos.418bae45.jpg"
+  },
+  {
+    "revision": "5dc8724c636a54f3ccd1b73e9825334c",
+    "url": "/Pokedex/static/media/Dragon.5dc8724c.jpg"
+  },
+  {
+    "revision": "81a2b999d929772d6c347dbbb16855da",
+    "url": "/Pokedex/static/media/HeaderPokemon.81a2b999.jpg"
+  },
+  {
+    "revision": "28cc8e884bbbf08ac3b44b4013a3d5db",
+    "url": "/Pokedex/static/media/Ivysaur.28cc8e88.png"
+  },
+  {
+    "revision": "03668fe50f3698c5faa5a78c269977e8",
+    "url": "/Pokedex/static/media/LinksBack.03668fe5.png"
+  },
+  {
+    "revision": "a43434413632e2bab5e0770e006452b8",
+    "url": "/Pokedex/static/media/LucPik.a4343441.png"
+  },
+  {
+    "revision": "be932ae0bf91d12078a5771d77ca2195",
+    "url": "/Pokedex/static/media/Omega.be932ae0.jpg"
+  },
+  {
+    "revision": "bb9ba4d9ce83ffae84c3205e7933e89d",
+    "url": "/Pokedex/static/media/PokeLogo.bb9ba4d9.png"
+  },
+  {
+    "revision": "7b9ddf692c6df64d86bbe4e96ef46a83",
+    "url": "/Pokedex/static/media/Squirtle.7b9ddf69.png"
+  },
+  {
+    "revision": "6084ac2b1e9235d28b7299f09de81a72",
+    "url": "/Pokedex/static/media/ThreeBirds.6084ac2b.jpg"
+  },
+  {
+    "revision": "8c37e2ae54a20df159b6706ef5663ff9",
+    "url": "/Pokedex/static/media/Venusaur.8c37e2ae.png"
+  },
+  {
+    "revision": "04cfac8634923b57a079cc4381035686",
+    "url": "/Pokedex/static/media/Wartortle.04cfac86.png"
+  },
+  {
+    "revision": "aaf34fe7f3ccc1b20db07331e8795525",
+    "url": "/Pokedex/static/media/slider-left-arrow.aaf34fe7.svg"
+  },
+  {
+    "revision": "1d292928e8fb1296e074fb9eb060de23",
+    "url": "/Pokedex/static/media/slider-right-arrow.1d292928.svg"
+  }
+]);
